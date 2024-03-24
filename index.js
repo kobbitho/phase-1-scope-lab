2cd7cc01fc7272228243ba1167d75028968c21fb
Tabithaokoth <kobbitho@gmail.com>
2024-03-24 12:41:33 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = ' maybe Noah';
+
+function setCustomerName() {
+  return customerName = 'bob';
+}
+
+function upperCaseCustomerName() {
+ return customerName = customerName.toUpperCase();
+
+}
+//console.log(upperCaseCustomerName());
+function setBestCustomer() {
+   return bestCustomer = 'not bob';
+}
+//console.log(setBestCustomer());
+function overwriteBestCustomer(){
+    return bestCustomer = 'maybe bob';
+}
+
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'maybe Noah'
+}
+
+
